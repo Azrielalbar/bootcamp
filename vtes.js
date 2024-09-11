d59827816d5286b6305a8a8ceb5fbd9cc86cb61c
Azriel <azrielalbar36@gmail.com>
2024-09-11 15:26:51 +0700

    'update'

--- vtes.js
+++ vtes.js
@@ -1,3 +1,47 @@
+// const readline = require("node:readline");
+// const { stdin: input, stdout: output } = require("node:process");
+// const { log } = require("node:console");
+// const fs = require("fs");
+// const validator = require("validator")
+
+// const rl = readline.createInterface({ input, output });
+
+// //membuat pertanyaan dan jawaban
+// rl.question("Siapa nama kamu? ", (nama) => {
+//     rl.question("Berapa nomor kamu? ", (nomor) => {
+//         rl.question("Apa email kamu? ", (email) => {
+//            const validemail = validator.isEmail(email);
+//            const validnomor = validator.isMobilePhone(nomor);
+//            if (validemail&&validnomor){
+//             const data = `Nama: ${nama}\nUmur: ${nomor}\nHobi: ${email}\n`;
+
+//             // Menyimpan data ke file
+//             fs.writeFile("tes2.txt", data, (err) => {
+//                 if (err) {
+//                     log("Terjadi kesalahan saat menyimpan data.");
+//                 } else {
+//                     log("Data berhasil disimpan ke file tes2.txt.");
+//                 }
+                
+//             });
+//         } else {
+//             if (!validemail){
+//                 console.log("email anda salah");
+                
+//             }
+//             if (!validnomor){
+//                 console.log("nomor anda salah");
+                
+//             }
+//         }
+//         rl.close();
+//         });
+//     });
+// });
+
+
+
+
 const readline = require("node:readline");
 const { stdin: input, stdout: output } = require("node:process");
 const { log } = require("node:console");
@@ -14,16 +58,14 @@ rl.question("Siapa nama kamu? ", (nama) => {
            const validnomor = validator.isMobilePhone(nomor);
            if (validemail&&validnomor){
             const data = `Nama: ${nama}\nUmur: ${nomor}\nHobi: ${email}\n`;
+            const result = { nama, nomor, email };
+            const file = fs.readFileSync("data/Contacts.json", "utf-8");
+            const contacts = JSON.parse(file);
+            contacts.push(result); 
 
             // Menyimpan data ke file
-            fs.writeFile("tes2.txt", data, (err) => {
-                if (err) {
-                    log("Terjadi kesalahan saat menyimpan data.");
-                } else {
-                    log("Data berhasil disimpan ke file tes2.txt.");
-                }
-                
-            });
+            fs.writeFileSync("data/contacts.json", JSON.stringify(contacts) )
+               
         } else {
             if (!validemail){
                 console.log("email anda salah");
@@ -38,3 +80,4 @@ rl.question("Siapa nama kamu? ", (nama) => {
         });
     });
 });
+
