01ba778ff12a55071df6dbba787c6edf349bcb92
Azriel <azrielalbar36@gmail.com>
2024-09-20 15:18:47 +0700

    'update'

--- Expressjs.js
+++ Expressjs.js
@@ -2,23 +2,38 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
+const fs = require("fs");
+
+app.set("view engine", "ejs");
 
 // Middleware untuk melayani file statis
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Route untuk halaman utama
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    const nama = "kogie6";
+    res.render('index', { nama, title: "Home" });
+});
+
+// Route untuk mengarahkan /index ke /
+app.get("/index", (req, res) => {
+    res.redirect("/");
 });
 
 // Route untuk halaman tentang
 app.get("/about", (req, res) => {
-    res.send("About Page");
+    res.render('about', { title: 'About Us' });
 });
 
 // Route untuk halaman kontak
-app.get("/contact", (req, res) => {
-    res.send("Contact Page");
+app.get('/contacts', (req, res) => {
+    fs.readFile(path.join(__dirname, 'contacts.json'), 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading contacts file.');
+        }
+        const contacts = JSON.parse(data);
+        res.render('contacts', { title: 'Contacts', contacts });
+    });
 });
 
 // Route untuk produk dengan kategori
