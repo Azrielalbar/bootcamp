9faa3100e2f6768d580a695cadc30a76da8ab81e
Azriel <azrielalbar36@gmail.com>
2024-10-04 13:14:25 +0700

    hari ke 18

--- bootcamp_reactjs/src/comment.jsx
+++ bootcamp_reactjs/src/comment.jsx
@@ -1,31 +1,50 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import commentsData from "./commentData";
-import {faker} from '@faker-js/faker'
 
-const Comment = () => {
-  return (
-    <div className="ui comments">
-      <h3 className="ui dividing header">Comments</h3>
-      {commentsData.map((comment, index) => (
-        <div className="comment" key={index}>
-          <a className="avatar">
-            <img src={comment.avatar} alt={`${comment.author}'s avatar`} />
-          </a>
-          <div className="content">
-            <a className="author">{comment.author}</a>
-            <div className="metadata">
-              <span className="date">{comment.date}</span>
-            </div>
-            <div className="text">{comment.comment}</div>
-            <div className="actions">
-              <a className="reply">Reply</a>
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+    // Inisialisasi state untuk jumlah like
+    this.state = {
+      likes: new Array(commentsData.length).fill(0), // Semua komentar dimulai dengan 0 likes
+    };
+  }
+
+  // Fungsi untuk menangani klik tombol like
+  handleLike = (index) => {
+    const newLikes = [...this.state.likes]; // Salin array likes
+    newLikes[index] += 1; // Tambah like pada komentar tertentu
+    this.setState({ likes: newLikes }); // Perbarui state dengan jumlah like baru
+  };
+
+  render() {
+    return (
+      <div className="ui comments">
+        <h3 className="ui dividing header">Comments</h3>
+        {commentsData.map((comment, index) => (
+          <div className="comment" key={index}>
+            <a className="avatar">
+              <img src={comment.avatar} alt={`${comment.author}'s avatar`} />
+            </a>
+            <div className="content">
+              <a className="author">{comment.author}</a>
+              <div className="metadata">
+                <span className="date">{comment.date}</span>
+              </div>
+              <div className="text">{comment.comment}</div>
+              <div className="actions">
+                <a className="reply">Reply</a>
+              </div>
+              <div className="actions">
+                <button onClick={() => this.handleLike(index)}>Like</button>
+                <span>{this.state.likes[index]} Likes</span>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Comment;
